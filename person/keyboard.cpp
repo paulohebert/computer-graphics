7834573cbd32b2c79527bc47a51b404ecc5f7f36
PH <paulo.hebert@discente.univasf.edu.br>
2024-05-03 12:52:39 -0300

    refactor: change controls

--- person/keyboard.cpp
+++ person/keyboard.cpp
@@ -6,28 +6,44 @@ void keyboard(unsigned char key, int x, int y)
     switch (key)
     {
     case 'w':
+    case 'W':
         if (rot_body >= -90)
             rot_body -= 10.0f;
         break;
-    case 's':
+    case 'q':
+    case 'Q':
         if (rot_body <= 90)
             rot_body += 10.0f;
         break;
-    case 'q':
+    case 'a':
+    case 'A':
         if (rot_left_arm >= -90)
             rot_left_arm -= 10.0f;
         break;
-    case 'a':
+    case 'z':
+    case 'Z':
         if (rot_left_arm <= 10)
             rot_left_arm += 10.0f;
         break;
-    case 'e':
+    case 's':
+    case 'S':
         if (rot_right_arm <= 90)
             rot_right_arm += 10.0f;
         break;
-    case 'd':
+    case 'x':
+    case 'X':
         if (rot_right_arm >= -10)
             rot_right_arm -= 10.0f;
+        break;
+    case 'd':
+    case 'D':
+        if (rot_sword <= 90)
+            rot_sword += 10.0f;
+        break;
+    case 'c':
+    case 'C':
+        if (rot_sword >= -10)
+            rot_sword -= 10.0f;
         break;
     }
     glutPostRedisplay();
